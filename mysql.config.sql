7cc28beb1f688d1698a337d57aa1c41edaf61869
vinifig <vinifig@hotmail.com>
2016-06-06 16:27:42 -0300

    Criação de Triggers, procedures, views e functions para a regra de negócio

--- mysql.config.sql
+++ mysql.config.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE IF NOT EXISTS guerreiro_app;
 use guerreiro_app;
 
-CREATE USER IF NOT EXISTS "guerreiro_app"@"localhost" IDENTIFIED BY "guerreiro123";
-
-GRANT ALL PRIVILEGES ON guerreiro_app.* TO "guerreiro_app"@"localhost";
-
-REVOKE CREATE ON guerreiro_app.* to "guerreiro_app"@"localhost";
-REVOKE DROP ON guerreiro_app.* to "guerreiro_app"@"localhost";
+-- CREATE USER IF NOT EXISTS "guerreiro_app"@"localhost" IDENTIFIED BY "guerreiro123";
+--
+-- GRANT ALL PRIVILEGES ON guerreiro_app.* TO "guerreiro_app"@"localhost";
+--
+-- REVOKE CREATE ON guerreiro_app.* to "guerreiro_app"@"localhost";
+-- REVOKE DROP ON guerreiro_app.* to "guerreiro_app"@"localhost";
 
 CREATE TABLE IF NOT EXISTS Cliente(
   num_celular varchar(20) primary key,
@@ -18,21 +18,20 @@ CREATE TABLE IF NOT EXISTS Cliente(
 
 CREATE TABLE IF NOT EXISTS Funcionario(
   cpf_funcionario varchar(14) primary key,
-  nivel_funcionario int default 1,
-  nome_funcionario varchar(50) not null
+  nome_funcionario varchar(50) not null,
+  nivel_funcionario int default 1
 );
 
 CREATE TABLE IF NOT EXISTS Ingrediente(
   codigo_ingrediente int primary key auto_increment,
-  nome_ingrediente varchar(50) not null,
-  quantidadeGasta int default 0
+  nome_ingrediente varchar(50) not null
 );
 
 CREATE TABLE IF NOT EXISTS ItemMenu(
   codigo_item int primary key auto_increment,
   nome_item varchar(50) not null,
   preco_item float(2,2) not null,
-  descricao_item varchar(50) not null
+  descricao_item TINYTEXT not null
 );
 
 
@@ -45,7 +44,7 @@ CREATE TABLE IF NOT EXISTS Pedido(
   codigo_pedido int primary key auto_increment,
   status int not null,
   observacoes_pedido TINYTEXT,
-  datahora_pedido DATETIME not null,
+  datahora_pedido DATETIME not null DEFAULT NOW(),
   datahora_entrega DATETIME null,
 
   forma_pagamento_fk int not null,
@@ -75,6 +74,13 @@ CREATE TABLE IF NOT EXISTS PedidoItem(
   Constraint pk_PedidoItem primary key(codigo_pedido, codigo_item),
   Constraint fk_PedidoItem_Pedido foreign key(codigo_pedido) references Pedido(codigo_pedido),
   Constraint fk_PedidoItem_Item foreign key(codigo_item) references ItemMenu(codigo_item)
+);
+
+CREATE TABLE IF NOT EXISTS IngredienteGasto(
+  codigo_ingrediente int not null,
+  quantidade_gasta int not null,
+  datahora_gasto DATETIME not null,
+  Constraint fk_IngredienteGasto_Ingrediente foreign key(codigo_ingrediente) references Ingrediente(codigo_ingrediente)
 );
 
 -- FUNCTIONS GERAIS
@@ -141,7 +147,7 @@ CREATE FUNCTION codigoPedidoAbertoCliente(telefone varchar(20)) RETURNS int
     DECLARE cpa int DEFAULT -1;
 
     SELECT Pedido.codigo_pedido into cpa
-      FROM [PedidosEmAberto]
+      FROM PedidosEmAberto
       WHERE
         cliente_fk like telefone;
 
@@ -185,22 +191,142 @@ DELIMITER $$
 
 DELIMITER ;
 
+
+-- PROCEDURES
+
+DELIMITER $$
+
+-- PROC DE CLIENTES
+
+CREATE PROCEDURE cria_cliente(IN nome VARCHAR(50), IN celular VARCHAR(20), IN senha VARCHAR(32), IN email VARCHAR(100) )
+  BEGIN
+    INSERT INTO Cliente values(celular, nome, senha, email, 1);
+  END $$
+
+CREATE PROCEDURE autoriza_cliente(IN celular VARCHAR(20))
+  BEGIN
+    UPDATE ClientesPendentes
+    SET status = 2
+    WHERE num_celular=celular;
+  END $$
+
+CREATE PROCEDURE remove_cliente(IN celular VARCHAR(20))
+  BEGIN
+    UPDATE Cliente
+    SET status = 3
+    WHERE num_celular=celular;
+  END $$
+
+-- PROC DE PEDIDOS
+
+CREATE PROCEDURE cria_pedido( IN telefone_cliente varchar(20), IN cpf_funcionario varchar(14), IN codigo_forma_pagamento int)
+  BEGIN
+    INSERT INTO
+      Pedido(status, forma_pagamento_fk, cliente_fk, funcionario_fk )
+      VALUES
+        (1, codigo_forma_pagamento, telefone_cliente, cpf_funcionario);
+  END $$
+
+CREATE PROCEDURE fecha_pedido( IN c_pedido int, IN dh_entrega DATETIME, IN obs TINYTEXT)
+  BEGIN
+    UPDATE PedidosEmAberto
+      SET status = 2, datahora_entrega=dh_entrega, observacoes_pedido = obs
+      WHERE codigo_pedido = c_pedido;
+  END $$
+
+CREATE PROCEDURE entrega_pedido( IN c_pedido int )
+  BEGIN
+    UPDATE PedidosConfirmados
+      SET status = 3
+      WHERE codigo_pedido = c_pedido;
+  END $$
+
+-- PROCs ItemMenu
+
+CREATE PROCEDURE adiciona_item_menu( IN preco float(2,2), IN nome VARCHAR(50), IN descricao TINYTEXT )
+  BEGIN
+    INSERT INTO ItemMenu(nome_item, preco_item, descricao_item)
+      VALUES(nome, preco, descricao);
+  END $$
+
+CREATE PROCEDURE remove_item_menu( IN codigo int )
+  BEGIN
+    DELETE FROM ItemMenu
+      WHERE codigo_item=codigo;
+  END $$
+
+-- PROCs Ingrediente
+
+CREATE PROCEDURE adiciona_ingrediente( IN nome varchar(50) )
+  BEGIN
+    INSERT INTO Ingrediente(nome_ingrediente)
+      VALUES(nome);
+  END $$
+
+CREATE PROCEDURE remove_ingrediente( IN codigo int )
+  BEGIN
+    DELETE FROM Ingrediente
+      Where codigo_ingrediente = codigo;
+  END $$
+
+-- PROCs ItemIngrediente
+
+CREATE PROCEDURE adiciona_ingrediente_item( IN ing_codigo int, IN item_codigo int, IN qtd int )
+  BEGIN
+    INSERT INTO ItemIngrediente
+      VALUES(item_codigo, ing_codigo, qtd);
+  END $$
+
+CREATE PROCEDURE remove_ingrediente_item( IN ing_codigo int, IN item_codigo int)
+  BEGIN
+    DELETE FROM ItemIngrediente
+      WHERE codigo_ingrediente = ing_codigo AND codigo_item = item_codigo;
+  END $$
+
+CREATE PROCEDURE atualiza_qtd_ingrediente_item( IN ing_codigo int, IN item_codigo int, IN qtd int )
+  BEGIN
+    UPDATE ItemIngrediente
+      SET quantidade = qtd
+      WHERE codigo_ingrediente = ing_codigo AND codigo_item = item_codigo;
+  END$$
+
+DELIMITER ;
+
 -- TRIGGERS
 
 DELIMITER $$
 
--- CREATE OR REPLACE TRIGGER  entrega_pedido
+CREATE OR REPLACE TRIGGER item_menu_adicionado
+  BEFORE INSERT ON PedidoItem
+  FOR EACH ROW
+  BEGIN
+    DECLARE pedido_status int;
+    SELECT Pedido.status INTO pedido_status
+      FROM Pedido
+      WHERE Pedido.codigo_pedido = new.codigo_pedido;
+    IF(pedido_status <> 1) THEN
+      SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "DIE: Pedidos fechados ou entregues não podem ter seus itens alterados";
+    END IF;
+  END $$
+
+-- CREATE OR REPLACE TRIGGER entrega_pedido
 --   AFTER UPDATE ON Pedido
 --   FOREACH row
 --   BEGIN
 --     IF(new.status = 3 AND old.status <> 3) THEN
+--       (SELECT ItemIngrediente.codigo_ingrediente, ItemIngrediente.quantidade
+--         FROM PedidoItem
+--         INNER JOIN ItemMenu ON  ItemMenu.codigo_pedido = new.codigo_pedido
+--         INNER JOIN ItemIngrediente ON ItemMenu.codigo_item = ItemIngrediente.codigo_item)
 --
+--     ENDIF;
 --
 --   END $$
+
 
 
 DELIMITER ;
 
 -- INSERTS
 
-INSERT INTO Funcionario values("none", "Pedido Online")
+INSERT INTO Funcionario(cpf_funcionario, nome_funcionario) values("none", "Pedido Online");
